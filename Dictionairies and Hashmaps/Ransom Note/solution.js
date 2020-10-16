c43f2a30feaaeb53507f1605cd61309a5b64d857
LycheeCocoa <33363125+LycheeCocoa@users.noreply.github.com>
2020-10-15 20:39:30 -0500

    First Commit

---
+++ Dictionairies and Hashmaps/Ransom Note/solution.js
@@ -0,0 +1,50 @@
+function checkMagazineLogic(magazine, note) {
+    const dict = buildHistogram(magazine);
+    // this is v annoying LOL /ff yea
+    // omg
+    // wait
+    // miku
+    // can u compile
+    // i dont get it lmao
+    // might need to save first
+    //wtf
+    // i need to see debugging mode lolol
+    for (const word of note) {
+        if (dict[word] && dict[word] >= 1) {
+            dict[word] = dict[word] - 1;
+        }
+        else {
+            return "No";
+        }
+    }
+    return "Yes";
+}
+// okok that makes sense
+// it may evaluate to 'undefined' which is probably falsy when compared to <= 1
+// nani the fuck it failed?
+// maybe our solution is wrong
+// bazinga still wrong
+function checkMagazine(magazine, note) {
+    console.log(checkMagazineLogic(magazine, note));
+}
+// ive got a lovely bunch of coconuts
+// ive got some coconuts
+// nani
+// why the fuck did this return true
+// so annoying i cant go into debugger lmao
+function buildHistogram(arr) {
+    const hist = {};
+    arr.forEach((word) => {
+        if (hist[word]) {
+            hist[word] = hist[word] + 1;
+        }
+        else {
+            hist[word] = 1;
+        }
+    });
+    return hist;
+}
+const magazine = ["two", "times", "three", "is", "not", "four"];
+const note = ["two", "times", "two", "is", "four"];
+console.log(checkMagazine(magazine, note));
+//# sourceMappingURL=solution.js.map
